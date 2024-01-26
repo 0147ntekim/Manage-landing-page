fc93f388b537e584b4d9df925b0933e4838e7a04
0147ntekim <147ntekim@gmail.com>
2024-01-26 09:45:38 +0100

    commit

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import illustration from "../images/illustration-intro.svg"
 const Home = () => {
     return ( 
         <div className="min-w-[100%] max-[720px]:min-h-[50vh] min-h-[70vh]  flex flex-col-reverse md:flex-row justify-between mt-[70px]">
-            <div className="w-[100%] md:w-[35%] max-[379px]:min-h-[60vh] max-[769px]:min-h-[50vh] min-[720px]:min-h-[20vh]  min-h-[40vh] md:min-h-[60vh] flex flex-col items-center md:items-start justify-between mt-[50px]">
+            <div className="w-[100%] md:w-[35%] max-[379px]:min-h-[65vh] max-[769px]:min-h-[50vh] min-[720px]:min-h-[20vh]  min-h-[40vh] md:min-h-[60vh] flex flex-col items-center md:items-start justify-between mt-[50px]">
                 <h1 className="text-[38px] font-semibold text-center md:text-start font-display text-[#242D52]">
                     Bring everyone together to build better products.
                 </h1>
--- src/header.js
+++ src/header.js
@@ -4,8 +4,8 @@ import bgtablet from "./images/bg-tablet-pattern.svg";
 
 const Header = () => {
     return ( 
-        <div className=" w-full max-[720px]:min-h-[75vh] min-h-screen px-[20px] sm:px-[50px] md:px-[150px] pt-[50px] relative overflow-x-hidden ">
-            <img className="w-[100%] md:w-[50%] h-[70%] md:h-[100%] z-[-1] absolute bottom-[54%] md:bottom-[15%] left-[30%] md:left-[55%]" src={bgtablet} alt="" />
+        <div className=" w-full max-[769px]:min-h-screen min-[720px]:min-h-[75vh] mdmin-h-screen px-[20px] sm:px-[50px] md:px-[150px] pt-[50px] relative overflow-x-hidden ">
+            <img className="w-[90%] md:w-[50%] h-[70%] md:h-[100%] z-[-1] absolute bottom-[40%] md:bottom-[15%] left-[20%] md:left-[55%]" src={bgtablet} alt="" />
             <Navbar/>
             <Home/>
         </div>
